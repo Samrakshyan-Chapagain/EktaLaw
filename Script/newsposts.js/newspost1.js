d8c47dfb851d24279f8bd2c610b31a52600f4055
Cereal <Cereal@gmail.com>
2025-03-13 12:28:26 +0545

    sixth commit

--- Script/newsposts.js/newspost1.js
+++ Script/newsposts.js/newspost1.js
@@ -1,34 +1,34 @@
 import { news } from "../news.js"
 
 document.querySelector('.home-sticker').addEventListener('click',()=>{
-  window.location.href = `../main.html`;
+  window.location.href = `main.html`;
 })
 
 document.querySelector('.about-us-sticker').addEventListener('click',()=>{
-  window.location.href='../about-us.html'
+  window.location.href='about-us.html'
 })
 
 document.querySelector('.our-team-sticker').addEventListener('click',()=>{
-  window.location.href='../our-team.html'
+  window.location.href='our-team.html'
 })
 
 document.querySelector('.dropdown-items').addEventListener('click',()=>{
-  window.location.href='../our-team.html'
+  window.location.href='our-team.html'
 })
 
 document.querySelector('.news-sticker').addEventListener('click',()=>{
-  window.location.href='../news.html'
+  window.location.href='news.html'
 })
 
 document.querySelector('.practice-areas-sticker').addEventListener('click',()=>{
-  window.location.href='../Practice.html'
+  window.location.href='Practice.html'
 })
 
 document.querySelector('.dropdown').addEventListener('click',()=>{
-  window.location.href='../Practice.html'
+  window.location.href='Practice.html'
 })
 
-document.querySelector('.news-2-div').innerHTML=`
+document.querySelector('.news-1-div').innerHTML=`
         <div style="font-size: 30px; font-weight: 700; width: 1000px; margin-left: 100px;">
           ${news[0].topic}
         </div>
